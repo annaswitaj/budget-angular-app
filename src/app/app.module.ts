830e7e32856191a75f983cef169ea6694a121522
Asia <joan.mk7@gmail.com>
2023-02-26 15:16:17 +0100

    create navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppComponent} from './app.component';
+import { AppComponent } from './app.component';
 
-import {AppRoutingModule} from './app-routing.module';
-import {NavbarComponent} from './layout/navbar/navbar.component';
-import {ContentLayoutComponent} from './layout/content-layout/content-layout.component';
-import {CoreModule} from './core/core.module';
-import {SharedModule} from './shared/shared.module';
+import { AppRoutingModule } from './app-routing.module';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { ContentLayoutComponent } from './layout/content-layout/content-layout.component';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, ContentLayoutComponent, NavbarComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule, SharedModule],
+  declarations: [
+    AppComponent,
+    ContentLayoutComponent,
+    NavbarComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CoreModule,
+    SharedModule,
+    NgbModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
