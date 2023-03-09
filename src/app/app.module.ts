2f81b2865941cde38d80081106badaf7290ea6b5
Asia <joan.mk7@gmail.com>
2023-03-09 23:02:15 +0100

    fixed navbar and responsiveness

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,14 +9,13 @@ import { ContentLayoutComponent } from './layout/content-layout/content-layout.c
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { FooterComponent } from './layout/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContentLayoutComponent,
     NavbarComponent,
-    FooterComponent
+
   ],
   imports: [
     BrowserModule,
